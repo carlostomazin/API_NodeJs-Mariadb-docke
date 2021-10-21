f166856c20c1b39e8c7a8b653765346cc1bdb29a
carlostomazin <carlos.tomazin2@gmail.com>
2021-10-20 23:43:21 -0300

    add referencia do swagger

--- src/server.js
+++ src/server.js
@@ -1,13 +1,31 @@
 const express = require('express')
-//const swaggerDocs = require('./swagger')
-const routes = require ('./routes')
-//const bodyParser = require('body-parser')
 const app = express()
+const routes = require ('./routes')
+
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+// https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'API Tech',
+            description: "Customer API information",
+            contact: {
+                name: "carlos"
+            },
+            servers: ["http://localhost:3000"]
+        }
+    },
+    apis: ['./src/routes.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.urlencoded({ extended: false})) // apenas dados simples
 app.use(express.json()) // json de entrada no body
 
-//app.use('/tech-docs', swaggerUi.server, swaggerUi.setup())
 app.use("/tech", routes)
 
 app.listen(3000, () => console.log("Server is running on port 3000 "))
