653947cc106d55b6ca423499024a00f17631b10a
Carlos Tomazin <65822302+carlostomazin@users.noreply.github.com>
2021-10-23 11:34:11 -0300

    Deleta parâmetros swagger

--- src/server.js
+++ src/server.js
@@ -2,27 +2,6 @@ const express = require('express')
 const app = express()
 const routes = require ('./routes')
 
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-
-// https://swagger.io/specification/#infoObject
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'API Tech',
-            description: "Customer API information",
-            contact: {
-                name: "carlos"
-            },
-            servers: ["http://localhost:3000"]
-        }
-    },
-    apis: ['./src/routes.js']
-}
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 app.use(express.urlencoded({ extended: false})) // apenas dados simples
 app.use(express.json()) // json de entrada no body
 
